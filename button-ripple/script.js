aba6701d997c18999fbf9f1bca80bb2bd6af1399
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-04-28 14:38:49 +0200

    changed js

--- button-ripple/script.js
+++ button-ripple/script.js
@@ -4,7 +4,7 @@ const btnEl = document.querySelector(".btn")
 // add event qilasan mouseover bolganida
 // x va y ni top
 //btnEl orqali set item qil style sifatida
-btnEl.addEventListener("mouseover",()=>{
+btnEl.addEventListener('mouseover',()=>{
     const x = (event.pageX - btnEl.offsetLeft)
     const y = (event.pageY - btnEl.offsetTop)
     btnEl.style.setProperty("--xPos", x + "px")
